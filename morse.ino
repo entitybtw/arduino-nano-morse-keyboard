e010550623906683f242127067e39243803dac83
entitybtw <studenov.ivan020312@gmail.com>
2026-01-09 14:30:25 +0300

    add 'morse' layout to morse.ino

--- morse.ino
+++ morse.ino
@@ -4,9 +4,8 @@ unsigned long lastMillis = 0;
 unsigned long pressStart = 0;
 bool lastState = false;
 String currentSymbol = "";
-bool isRussian = false;
+byte layoutMode = 0;
 bool layoutSignaled = false;
-
 
 struct MorseLetter {
   const char* code;
@@ -32,11 +31,34 @@ MorseLetter morseRU[] = {
   {".-.-.", "z"}
 };
 
-const char* decodeMorse(String s, bool rus) {
-  MorseLetter* table = rus ? morseRU : morseEN;
-  int len = rus ? sizeof(morseRU) / sizeof(MorseLetter) : sizeof(morseEN) / sizeof(MorseLetter);
-  for (int i = 0; i < len; i++) {
-    if (s == table[i].code) return table[i].letter;
+MorseLetter morseMORSE[] = {
+  {".-", ".-"},   {"-...", "-..."}, {"-.-.", "-.-."}, {"-..", "-.."},  {".", "."},
+  {"..-.", "..-."}, {"--.", "--."},  {"....", "...."}, {"..", ".."},   {".---", ".---"},
+  {"-.-", "-.-"},  {".-..", ".-.."}, {"--", "--"},   {"-.", "-."},   {"---", "---"},
+  {".--.", ".--."}, {"--.-", "--.-"}, {".-.", ".-."},  {"...", "..."},  {"-", "-"},
+  {"..-", "..-"},  {"...-", "...-"}, {".--", ".--"},  {"-..-", "-..-"}, {"-.--", "-.--"},
+  {"--..", "--.."}
+};
+
+const char* decodeMorse(String s, byte mode) {
+  if(mode == 0) {
+    MorseLetter* table = morseEN;
+    int len = sizeof(morseEN) / sizeof(MorseLetter);
+    for (int i = 0; i < len; i++) {
+      if (s == table[i].code) return table[i].letter;
+    }
+  } else if(mode == 1) {
+    MorseLetter* table = morseRU;
+    int len = sizeof(morseRU) / sizeof(MorseLetter);
+    for (int i = 0; i < len; i++) {
+      if (s == table[i].code) return table[i].letter;
+    }
+  } else if(mode == 2) {
+    MorseLetter* table = morseMORSE;
+    int len = sizeof(morseMORSE) / sizeof(MorseLetter);
+    for (int i = 0; i < len; i++) {
+      if (s == table[i].code) return table[i].letter;
+    }
   }
   return "?";
 }
@@ -53,24 +75,30 @@ void loop() {
   if (isPressed) {
     NewTone(8, 2000);
     if (!lastState) pressStart = now;
-    pressStart = now;
     layoutSignaled = false;
-
   } else {
     noNewTone(8);
     if (lastState) {
       unsigned long pressDuration = now - pressStart;
 
-      if (!layoutSignaled && now - pressStart >= 600) {
-        isRussian = !isRussian;
-        // note: надо чтобы предварительно на устройстве стояла en раскладка
-        Keyboard.press(KEY_LEFT_ALT);
-        Keyboard.press(KEY_LEFT_SHIFT);
-        Keyboard.releaseAll();
-        noNewTone(8);
+      if (!layoutSignaled && pressDuration >= 600) {
+        layoutMode++;
+        if(layoutMode > 2) layoutMode = 0;
+        
+        if(layoutMode == 0 || layoutMode == 1) {
+          Keyboard.press(KEY_LEFT_ALT);
+          Keyboard.press(KEY_LEFT_SHIFT);
+          Keyboard.releaseAll();
+        } else {
+          Keyboard.press(KEY_LEFT_GUI);
+          Keyboard.press(' ');
+          delay(100);
+          Keyboard.releaseAll();
+        }
+        
         NewTone(8, 1000);
         delay(50);
-        noNewTone(8)
+        noNewTone(8);
         layoutSignaled = true;
       } else if (pressDuration >= 200) {
         currentSymbol += "-";
@@ -82,7 +110,7 @@ void loop() {
     }
 
     if (currentSymbol.length() && now - lastMillis > 600) {
-      Keyboard.print(decodeMorse(currentSymbol, isRussian));
+      Keyboard.print(decodeMorse(currentSymbol, layoutMode));
       currentSymbol = "";
     }
   }
